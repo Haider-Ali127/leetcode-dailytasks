b9156aa48495f9b447562acb541790d3346bbe60
Haider Ali <haidet0000@gmail.com>
2026-03-31 21:24:07 +0500

    Time: 0 ms (100%), Space: 54.9 MB (83.94%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var trap = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let total = 0;
+    let leftMax = 0;
+    let rightMax = 0;
+    while (left < right) {
+        if (height[left] < height[right]) {
+            if (height[left] >= leftMax) {
+                leftMax = height[left];
+            } else {
+                total += leftMax - height[left];
+            }
+            left++;
+        } else {
+            if (height[right] >= rightMax) {
+                rightMax = height[right];
+            } else {
+                total += rightMax - height[right];
+            }
+            right--;
+        }
+    }
+    return total;
+};
