decc48088fe1ba3c0d6a995ce4994f5f3bb4112a
Haider Ali <haidet0000@gmail.com>
2026-04-13 16:21:54 +0500

    Time: 4 ms (51.21%), Space: 54.8 MB (85.31%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = []
+    for(let i = 0; i < s.length; i++){
+        if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+            stack.push(s[i])
+        } 
+        else if(s[i] == ')' || s[i] == ']' || s[i] == '}'){
+            if(stack.length == 0){
+                return false
+            }   
+            if(
+                (s[i] == ')' && stack.pop() !== '(') ||
+                (s[i] == ']' && stack.pop() != '[') ||
+                (s[i] == '}' && stack.pop() != '{'))
+                {
+                return false
+            }
+        }
+    }
+    return stack.length == 0 
+};
