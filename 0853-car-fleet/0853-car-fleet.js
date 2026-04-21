5f3ca39a6e4e303c8c29d06de50a54f160797972
Haider Ali <haidet0000@gmail.com>
2026-04-21 20:19:16 +0500

    Time: 194 ms (50.1%), Space: 92.6 MB (21.06%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.js
+++ 0853-car-fleet/0853-car-fleet.js
@@ -5,26 +5,17 @@
  * @return {number}
  */
 var carFleet = function(target, position, speed) {
-    let n = position.length;
-    let cars = new Array(n);
-    
-    for (let i = 0; i < n; i++) {
-        cars[i] = [position[i], speed[i]];
-    }
-
+    let cars = position.map((pos, i) => [pos, speed[i]]);
     cars.sort((a, b) => b[0] - a[0]);
-
     let fleets = 0;
     let lastTime = 0;
-
-    for (let i = 0; i < n; i++) {
-        let time = (target - cars[i][0]) / cars[i][1];
-
+    for (let [pos, spd] of cars) {
+        let time = (target - pos) / spd;
         if (time > lastTime) {
             fleets++;
             lastTime = time;
         }
     }
-
+    
     return fleets;
 };
