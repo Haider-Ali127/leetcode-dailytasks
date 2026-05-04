6854f062ced0ce7b1af72e0777d9569ee8e8afa4
Haider Ali <haidet0000@gmail.com>
2026-05-04 15:45:42 +0500

    Time: 0 ms (100%), Space: 58.2 MB (48.17%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,21 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let left = 0
-    let right = nums.length - 1  //  
-    
+    let left = 0;
+    let right = nums.length - 1
+
     while(left <= right){
-        let mid = Math.floor(left + (right - left) / 2)  
-        
-        if(nums[mid] == target){
-            return mid
-        }
-        if(nums[mid] < target){
-            left = mid + 1
-        }
-        else {
-            right = mid - 1
-        }
+    let mid = Math.floor(left + (right - left)/2)
+    if(nums[mid] == target){
+        return mid
+    }else if(nums[mid] < target){
+        left = mid + 1
+    }
+    else {
+        right = mid - 1
+    }
     }
     return -1
 }
+
