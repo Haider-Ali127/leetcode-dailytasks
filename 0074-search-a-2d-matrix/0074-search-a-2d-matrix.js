fe263c374a3a8833cc65d0cc0a6a3efdd23a5d7e
Haider Ali <haidet0000@gmail.com>
2026-05-11 19:32:59 +0500

    Time: 0 ms (100%), Space: 54.7 MB (14.71%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let m = matrix.length;
+    let n = matrix[0].length;
+    let left = 0;
+    let right = m * n - 1;
+    while(left <= right){
+        let mid = Math.floor(left + (right - left) / 2);
+        let midValue = matrix[Math.floor(mid / n)][mid % n];
+        if(midValue == target){
+            return true;
+        }
+        else if(midValue < target){
+            left = mid + 1;
+        }
+        else{
+            right = mid - 1;
+        }
+    }
+    return false;
+};
+
+
+ 
