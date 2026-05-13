ccd0f9894f87715808a1fcbaf1aa08cb5fd2b66b
Haider Ali <haidet0000@gmail.com>
2026-05-13 19:48:03 +0500

    Time: 0 ms (100%), Space: 54.1 MB (39.17%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -4,12 +4,27 @@
  * @return {number}
  */
 var search = function(nums, target) {
-   let right = nums.length
-
-   for(let i =0; i < right ; i++){
-    if(nums[i] == target){
-        return i
+    let left = 0;
+    let right = nums.length - 1;
+    while (left <= right) {
+        let mid = Math.floor(left + (right - left) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        }
+        if (nums[left] <= nums[mid]) {
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } 
+        else {
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
     }
-   }
-   return -1
-}
+    return -1;
+};
