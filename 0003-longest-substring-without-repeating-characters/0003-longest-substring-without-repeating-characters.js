a3534ac4c098a3f0859408bd79664674e39aa51b
Haider Ali <haidet0000@gmail.com>
2026-05-19 15:35:53 +0500

    Time: 6 ms (65.34%), Space: 58 MB (58.77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.js
@@ -3,16 +3,17 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let set = new Set();
-    let maxLength = 0;
-    let left = 0;
-    for(let right = 0; right < s.length; right++){
+    let set = new Set()
+    let maxLength = 0
+    let left = 0
+
+    for(let right = 0; right < s.length ; right++){
         while(set.has(s[right])){
             set.delete(s[left]);
-            left++;
+            left++
         }
-        set.add(s[right]);
-        maxLength = Math.max(maxLength, right - left + 1);
+        set.add(s[right])
+        maxLength = Math.max(maxLength, right - left + 1)
     }
     return maxLength;
 };
