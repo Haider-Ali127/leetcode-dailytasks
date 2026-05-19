8661ba0a4848eba93c9dcd80a378c364c024a893
Haider Ali <haidet0000@gmail.com>
2026-05-19 15:18:18 +0500

    Time: 2 ms (65.87%), Space: 55.3 MB (40.75%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -1,11 +1,16 @@
- let twoSum = function(nums, target) {
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] + nums[j] === target) {
-                return [i, j];
-            }
-        }
-    }
-};
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for(let i = 0; i < nums.length; i++){
+        let complement = target - nums[i];
 
-console.log(twoSum([2,7,11,15], 9));
+        if(map.has(complement)){
+            return [map.get(complement), i]
+        }
+        map.set(nums[i], i)
+    }
+}
