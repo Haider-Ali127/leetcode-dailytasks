6f0ca35c818ce2376b577fd5bdfe7c1cff48fca2
Haider Ali <haidet0000@gmail.com>
2026-05-20 19:07:41 +0500

    Time: 1 ms (75.84%), Space: 58.9 MB (89.12%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,13 +3,13 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let k = 0
-    for(let right = 0; right < nums.length; right++){
-        if(nums[right] !== 0){
-            let temp = nums[k];
-            nums[k] = nums[right]
-            nums[right] = temp
-            k++;
-        }
+   let k = 0;
+   for(let right = 0; right < nums.length;right++){
+    if(nums[right] !== 0){
+        let temp = nums[k]
+        nums[k] = nums[right]
+        nums[right] = temp
+        k++
     }
+   }
 };
