f5c24a034ef925665e8162126f6f41151ab738e0
Haider Ali <haidet0000@gmail.com>
2026-05-20 19:02:31 +0500

    Time: 8 ms (27.36%), Space: 60.1 MB (32.83%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -6,21 +6,18 @@ var isPalindrome = function(s) {
     let left = 0;
     let right = s.length - 1;
 
-    while (left < right) {
-        while (left < right && !(/[a-zA-Z0-9]/.test(s[left]))) {
-            left++;
+    while(left < right){
+        while(left < right && !(/[a-zA-Z0-9]/.test(s[left]))){
+            left++
         }
-        while (left < right && !(/[a-zA-Z0-9]/.test(s[right]))) {
-            right--;
+        while(left < right && !(/[a-zA-Z0-9]/.test(s[right]))){
+            right--
         }
-        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
-            return false;
+          if (s[left].toLowerCase() !== s[right].toLowerCase()){
+            return false
         }
         left++;
         right--;
     }
-    return true;
+    return true
 };
-
-
- 
