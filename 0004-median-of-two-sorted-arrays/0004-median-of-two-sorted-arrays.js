02ce4e702d022226e3fc2222bb7fb8723c119bb1
Haider Ali <haidet0000@gmail.com>
2026-06-03 15:05:27 +0500

    Time: 2 ms (85.97%), Space: 56.5 MB (96.61%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -4,33 +4,27 @@
  * @return {number}
  */
 var findMedianSortedArrays = function(nums1, nums2) {
-    
     let n1 = nums1.length;
     let n2 = nums2.length;
     let total = n1 + n2;
-
-    let mid = Math.floor(total / 2);
-
+    let mid = Math.floor(total/2)
     let i = 0, j = 0;
-    let prev = 0, curr = 0;
-
-    for (let k = 0; k <= mid; k++) {
-
+    let curr = 0, prev = 0;
+    for(let k = 0; k <= mid; k++){
         prev = curr;
-
-        if (i < n1 && (j >= n2 || nums1[i] <= nums2[j])) {
-            curr = nums1[i];
-            i++;
+        if (i < n1 && (j >= n2 || nums1[i] <= nums2[j])){
+            curr = nums1[i]
+            i++
         } else {
-            curr = nums2[j];
+            curr = nums2[j]
             j++;
         }
     }
-
-    if (total % 2 === 0) {
-        return (prev + curr) / 2;
+    if(total % 2 === 0){
+        return (prev + curr)/2
     } else {
-        return curr;
+        return curr
     }
+
 }
- 
+    
