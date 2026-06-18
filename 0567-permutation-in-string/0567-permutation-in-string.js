d089b00f201df4820565ec5feff28c1470b781d0
Haider Ali <haidet0000@gmail.com>
2026-06-18 17:26:00 +0500

    Time: 10 ms (80.6%), Space: 57 MB (69.93%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -5,33 +5,23 @@
  */
 var checkInclusion = function(s1, s2) {
     if (s1.length > s2.length) return false;
-
-    let freq = new Array(26).fill(0);
-
-    for (let ch of s1) {
+   let freq = new Array(26).fill(0);
+     for (let ch of s1) {
         freq[ch.charCodeAt(0) - 97]++;
     }
-
     let left = 0;
     let count = s1.length;
-
     for (let right = 0; right < s2.length; right++) {
         let r = s2.charCodeAt(right) - 97;
-
         if (freq[r] > 0) count--;
         freq[r]--;
-
         if (count === 0) return true;
-
         if (right - left + 1 === s1.length) {
             let l = s2.charCodeAt(left) - 97;
-
             if (freq[l] >= 0) count++;
             freq[l]++;
-
             left++;
         }
     }
-
     return false;
 };
