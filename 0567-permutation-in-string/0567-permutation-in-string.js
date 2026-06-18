8e5ed681e164fb9458762f055a9e732dc18b3e62
Haider Ali <haidet0000@gmail.com>
2026-06-18 15:53:10 +0500

    Time: 6 ms (97.27%), Space: 56.5 MB (75.39%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -4,31 +4,34 @@
  * @return {boolean}
  */
 var checkInclusion = function(s1, s2) {
-   let s1map = {}
-    let windowMap = {};
-    let left = 0;
-    let matchCount = 0;
-    let windowSize = s1.length
+    if (s1.length > s2.length) return false;
 
-   for(let char of s1){
-    s1map[char] = (s1map[char] || 0 ) + 1
-   }
-  for(let right = 0; right < s2.length; right++){
-    let char = s2[right]
+    let freq = new Array(26).fill(0);
 
-    if(s1map[char] !== undefined){
-        windowMap[char] = (windowMap[char] || 0) + 1;
-        if(s1map[char] == windowMap[char]) matchCount++;
+    for (let ch of s1) {
+        freq[ch.charCodeAt(0) - 97]++;
     }
-     if(right - left + 1 > windowSize){
-            let leftchar = s2[left]
-            if(s1map[leftchar] !== undefined){
-                if(windowMap[leftchar] === s1map[leftchar]) matchCount--;
-                windowMap[leftchar]--;
-            }
+
+    let left = 0;
+    let count = s1.length;
+
+    for (let right = 0; right < s2.length; right++) {
+        let r = s2.charCodeAt(right) - 97;
+
+        if (freq[r] > 0) count--;
+        freq[r]--;
+
+        if (count === 0) return true;
+
+        if (right - left + 1 === s1.length) {
+            let l = s2.charCodeAt(left) - 97;
+
+            if (freq[l] >= 0) count++;
+            freq[l]++;
+
             left++;
-   }
-   if(matchCount === Object.keys(s1map).length) return true;
-   }
-   return false;
-}
+        }
+    }
+
+    return false;
+};
