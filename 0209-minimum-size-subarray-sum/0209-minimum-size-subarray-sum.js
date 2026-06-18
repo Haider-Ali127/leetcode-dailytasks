1d31312e4c39fc3eb947d3bde555547a86f0e802
Haider Ali <haidet0000@gmail.com>
2026-06-18 17:32:02 +0500

    Time: 2 ms (86.91%), Space: 58.1 MB (64.47%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.js
@@ -5,20 +5,19 @@
  */
 var minSubArrayLen = function(target, nums) {
     let left = 0;
-     let sum = 0;
-     let mnLen = Infinity;
-
-     for(let right = 0; right < nums.length; right++){
-        sum += nums[right];
+    let sum = 0;
+    let mnLen = Infinity
+    for(let right = 0; right < nums.length; right++){
+        sum += nums[right]
         while(sum >= target){
             let window = right - left + 1;
-            mnLen = Math.min(mnLen, window);
-            sum -= nums[left];
+            mnLen = Math.min(mnLen, window)
+            sum -= nums[left]
             left++
-            }  
         }
-            if(mnLen == Infinity){
-                return 0;
-            }
-        return mnLen;
+    }
+    if(mnLen == Infinity){
+        return 0
+    }
+    return mnLen
 };
