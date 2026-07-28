0cd2b8a7f82f95145fd70cee49caecf11a9d32c8
Haider Ali <haidet0000@gmail.com>
2026-07-28 20:18:50 +0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.js
@@ -5,18 +5,16 @@
  */
 var topKFrequent = function(nums, k) {
     let map = {}
-
     for(let char of nums){
-        map[char] = (map[char] || 0) + 1
+        map[char] = [map[char] || 0] + 1;
+
     }
+    let arr = Object.entries(map)
+    arr.sort((a,b) => b[1] - a[1]);
 
-     let arr = Object.entries(map);
-
-    arr.sort((a, b) => b[1] - a[1]);
-
-    let result = [];
-    for(let i = 0; i < k; i++){
+    let result = []
+    for(let i = 0; i < k ; i++){
         result.push(Number(arr[i][0]))
     }
-    return result;
+    return result
 };
