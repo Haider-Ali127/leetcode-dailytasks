edddef96b48b118712376b5de80c0d6b814f710e
Haider Ali <haidet0000@gmail.com>
2026-07-28 20:08:51 +0500

    Time: 6 ms (93.91%), Space: 55.1 MB (86.42%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -4,11 +4,11 @@
  * @return {boolean}
  */
 var isAnagram = function(s, t) {
-     if(s.length !== t.length) return false;
-     const alpha = new Array(26).fill(0);
-     for(let i = 0; i < s.length; i++){
+      if(s.length !== t.length) return false
+      const alpha = new Array(26).fill(0)
+      for(let i = 0; i < s.length; i++){
         alpha[s[i].charCodeAt(0) - 97]++
         alpha[t[i].charCodeAt(0) - 97]--
-     }
-    return  alpha.every(x => x === 0)
+      }
+     return alpha.every(x => x == 0);
 };
