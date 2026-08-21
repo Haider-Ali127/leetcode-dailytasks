e8c0f937ba52a002d272d23695e0d19727c91a0e
Haider Ali <haidet0000@gmail.com>
2026-08-21 19:23:29 +0500

    Time: 0 ms (100%), Space: 54.6 MB (46.3%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -11,18 +11,18 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    let dummy = new ListNode(0);
+    let dummy = new ListNode(0)
     dummy.next = head;
     let slow = dummy;
     let fast = dummy;
     for (let i = 0; i < n; i++) {
         fast = fast.next;
     }
-    while (fast.next !== null) {
-        slow = slow.next;
-        fast = fast.next;
-    }
-    slow.next = slow.next.next;
+     while(fast.next !== null){
+        slow = slow.next
+        fast = fast.next
+     }
+    slow.next = slow.next.next
 
     return dummy.next;
 };
