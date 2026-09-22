3ecdd76c0953b2185da16b5c6945652808e61477
Haider Ali <haidet0000@gmail.com>
2026-09-22 22:41:20 +0500

    Time: 51 ms (22.44%), Space: 57.4 MB (29.56%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.js
@@ -6,20 +6,20 @@
 var characterReplacement = function(s, k) {
     let left = 0;
     let maxLength = 0;
-    let maxfrequency = 0;
-    let frequency = [];
+    let maxfrequency = 0    
+    let frequency = []
 
-    for(let right = 0; right  < s.length; right++){
-        let character = s[right];
+    for(let right = 0; right < s.length; right++){
+        let character = s[right]
         frequency[character] = (frequency[character] || 0) + 1
         maxfrequency = Math.max(maxfrequency, frequency[character])
-
-         windowLength = right - left + 1
-        if(windowLength - maxfrequency > k){
-            frequency[s[left]]--;
-            left++;
+    
+    let window = right - left + 1;
+    if(window - maxfrequency > k){
+        frequency[s[left]]--
+        left++
     }
-    maxLength = Math.max(maxLength, right - left + 1);
+    maxLength = Math.max(maxLength, right - left + 1)
     }
-    return maxLength;
+    return maxLength
 };
